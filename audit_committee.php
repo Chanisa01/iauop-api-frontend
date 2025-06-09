a8d0c055a5df8168dc70dda5129a936d1941b60c
chanisa01 <chanisa.iml@gmail.com>
2025-06-09 15:56:03 +0700

    Change api commitee layout

--- audit_committee.php
+++ audit_committee.php
@@ -10,11 +10,13 @@
     }
 
     $sql = "SELECT dc.group_year_start, dc.group_year_end, dc.title, dc.file_name,
-            ac.prename, ac.name, ac.surname, ac.image_committee_name, ac.position1, ac.position2
+            ac.prename, ac.name, ac.surname, ac.image_committee_name, ac.position1, ac.position2,
+            acg.group_name, acg.display_order
             FROM document_composition dc
             LEFT JOIN audit_committee ac ON ac.group_year_start = dc.group_year_start AND ac.group_year_end = dc.group_year_end
+            LEFT JOIN audit_committee_group acg ON acg.id = ac.position1
             WHERE dc.is_active = 1 AND ac.is_active = 1
-            ORDER BY dc.group_year_start DESC";
+            ORDER BY dc.group_year_start DESC , acg.display_order ASC";
 
     $result = $conn->query($sql);
 
@@ -42,7 +44,9 @@
                 'surname' => $row['surname'],
                 'image_committee_name' => $row['image_committee_name'],
                 'position1' => $row['position1'],
-                'position2' => $row['position2']
+                'position2' => $row['position2'],
+                'group_name' => $row['group_name'],
+                'display_order' => $row['display_order'],
             ];
         }
     }
